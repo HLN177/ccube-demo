15fd859b57d9ffc3c0ecde5caee2f3abbee76050
HLN177 <harryleen177@gmail.com>
2025-01-15 14:14:20 +0000

    chore

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       ],
       imports: [
         'react',
+        'vitest'
       ],
       dts: './auto-imports.d.ts'
     }),
@@ -30,5 +31,5 @@ export default defineConfig({
       '.ts',
       '.tsx',
     ]
-  },
+  }
 })
